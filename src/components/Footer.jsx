eec3025ca95bd1dcdf00e7e433a2422535278204
Vasu Sharma <vasu.sharma1768934@gmail.com>
2025-01-01 09:43:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-gray-800 to-gray-600 text-white py-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <div className="text-center md:text-left">
-          <p>Copyright © 2024-25, DD Robocon</p>
+          <p>Love you Asmit bhai</p>
         </div>
         <div className="text-center md:text-right">
           <p>Website created and designed by <a href="mailto:vasu.sharma1768934@gmail.com" className="underline">Vasu Sharma</a></p>
